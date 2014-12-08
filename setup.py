49584060ccb6eebe8710e6cd855ccec5b14878d4
Dimas Abreu Dutra <dimasadutra@gmail.com>
2014-12-07 22:28:19 -0500

    Package skeleton.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="qwfilter",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["numpy"],
+    
+    # metadata for upload to PyPI
+    author="Dimas Abreu Dutra",
+    author_email="dimasadutra@gmail.com",
+    description="QW filtering library.",
+    license="MIT",
+    url="http://github.com/dimasad/qwfilter",
+)
