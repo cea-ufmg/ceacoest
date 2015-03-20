kalman: corrected transform derivative errors.

qwfilter/kalman.py
@@ -17,6 +17,7 @@ Improvement ideas
 
 import abc
 import collections
+import re
 
 import numpy as np
 import numpy.ma as ma
@@ -317,7 +318,7 @@ class UnscentedTransform:
         self.in_sigma = self.in_dev + mean[..., None, :]
         return self.in_sigma
     
-    def unscented_transform(self, f, mean, cov):
+    def transform(self, f, mean, cov):
         in_sigma = self.gen_sigma_points(mean, cov)
         weights = self.weights
         
@@ -373,20 +374,30 @@ class UnscentedTransform:
         
         in_sigma_diff = self.sigma_points_diff(mean_diff, cov_diff)
         out_sigma_diff = f_diff(in_sigma, in_sigma_diff)
-        out_mean_diff = np.tensordot(out_sigma_diff, weights, axes=(0, 0))
-        out_dev_diff = out_sigma_diff - out_mean_diff
-        out_cov_diff = np.einsum('k...i,k...j,k->...ij', 
+        out_mean_diff = np.dot(weights, out_sigma_diff)
+        out_dev_diff = out_sigma_diff - out_mean_diff[..., None, :]
+        out_cov_diff = np.einsum('...ki,...kj,k->...ij',
                                  out_dev_diff, out_dev, weights)
-        out_cov_diff += np.einsum('k...i,k...j,k->...ij', 
+        out_cov_diff += np.einsum('...ki,...kj,k->...ij',
                                   out_dev, out_dev_diff, weights)
         return (out_mean_diff, out_cov_diff)
 
 
-class DTUnscentedPredictor(DTKalmanFilterBase, UnscentedTransform):
+class DTUnscentedPredictor(DTKalmanFilterBase):
+
+    def __init__(self, model, mean, cov, **options):
+        super().__init__(model, mean, cov, **options)
+
+        ut_options = options.copy()
+        for key, val in options.items():
+            match = re.match('pred_ut_(?P<subkey>\w+)', key)
+            if match:
+                ut_options[match.group('subkey')] = val
+        
+        self.__ut = UnscentedTransform(model.nx, **ut_options)
     
     def predict(self, u=[]):
         '''Predict the state distribution at the next time index.'''
-        
         aug_mean = np.concatenate([self.mean, np.zeros(self.model.nw)])
         aug_cov = scipy.linalg.block_diag(self.cov, self.model.wcov)
         def faug(xaug):
@@ -394,7 +405,7 @@ class DTUnscentedPredictor(DTKalmanFilterBase, UnscentedTransform):
             w = xaug[self.model.nx:]
             return self.model.f(self.k, x, u, w)
         
-        pred_mean, pred_cov = self.unscented_transform(faug, aug_mean, aug_cov)
+        pred_mean, pred_cov = self.transform(faug, aug_mean, aug_cov)
         self._save_prediction(pred_mean, pred_cov)
         
         if self.calculate_gradients:
@@ -449,7 +460,7 @@ class DTUnscentedCorrector(DTKalmanFilterBase, UnscentedTransform):
             return self.model.h(self.k, x, u)[active]
         
         #Perform unscented transform
-        hmean, hcov = self.unscented_transform(meas_fun, self.mean, self.cov)
+        hmean, hcov = self.transform(meas_fun, self.mean, self.cov)
         hcrosscov = self.transform_crosscov()
         
         #Factorize covariance

qwfilter/tests/test_kalman.py
@@ -153,7 +153,7 @@ def test_cholesky_sqrt_diff(cov, nx):
     jac = kalman.cholesky_sqrt_diff(S)
     for i, j in np.ndindex(nx, nx):
         numerical = utils.central_diff(f, 0)
-        assert ArrayCmp(jac[i, j], atol=1e-7) == numerical
+        assert ArrayCmp(jac[i, j], tol=1e-7) == numerical
         
         dQ = np.zeros((nx, nx))
         dQ[i, j] = 1
@@ -176,7 +176,7 @@ def test_sigma_points(ut, x, cov):
 def test_affine_ut(ut, x, cov, A, nx):
     '''Test the unscented transform of an affine function.'''
     f = lambda x: np.dot(x, A.T) + np.arange(nx)
-    [ut_mean, ut_cov] = ut.unscented_transform(f, x, cov)
+    [ut_mean, ut_cov] = ut.transform(f, x, cov)
     
     desired_mean = f(x)
     assert ArrayCmp(ut_mean) == desired_mean
@@ -197,10 +197,10 @@ def test_sigma_points_diff_wrt_mean(ut, ut_sqrt, x, cov, nx):
     def sigma(mean, cov):
         return ut.gen_sigma_points(mean, cov)
     
-    ds_dmean_num = utils.central_diff(lambda x: sigma(x, cov), x)
-    
     sigma(x, cov)
     ds_dmean = ut.sigma_points_diff(np.identity(nx), np.zeros((nx, nx, nx)))
+    ds_dmean_num = utils.central_diff(lambda x: sigma(x, cov), x)
+    
     assert ArrayCmp(ds_dmean_num) == ds_dmean
 
 
@@ -235,27 +235,51 @@ def test_transform_diff_wrt_q(ut, ut_sqrt, nlfunction, x, q, cov, nx, nq):
     if ut_sqrt == 'svd':
         pytest.skip("`svd_sqrt_diff` not implemented yet.")
     
-    pytest.skip()
-    
     def ut_mean(q):
-        return ut.unscented_transform(lambda x: nlfunction(x, q), x, cov)[0]
+        return ut.transform(lambda x: nlfunction(x, q), x, cov)[0]
     
     def ut_cov(q):
-        return ut.unscented_transform(lambda x: nlfunction(x, q), x, cov)[1]
-    
-    num_mean_diff = utils.central_diff(ut_mean, q)
-    num_cov_diff = utils.central_diff(ut_cov, q)
+        return ut.transform(lambda x: nlfunction(x, q), x, cov)[1]
     
     def f_diff(x, dx):
         return nlfunction.d_dq(x, q)
     
+    num_mean_diff = utils.central_diff(ut_mean, q)
+    num_cov_diff = utils.central_diff(ut_cov, q)
+
+    ut_mean(q)
     in_mean_diff = np.zeros((nq, nx))
     in_cov_diff = np.zeros((nq, nx, nx))
-    ut.unscented_transform(lambda x: nlfunction(x, q), x, cov)
     mean_diff, cov_diff = ut.transform_diff(f_diff, in_mean_diff, in_cov_diff)
     
-    assert ArrayCmp(mean_diff) == num_mean_diff
-    assert ArrayCmp(cov_diff) == num_cov_diff
+    assert ArrayCmp(mean_diff, tol=2e-6) == num_mean_diff
+    assert ArrayCmp(cov_diff, tol=2e-6) == num_cov_diff
+
+
+def test_transform_diff_wrt_mean(ut, ut_sqrt, nlfunction, x, q, cov, nx):
+    '''Test the derivatives of unscented transform.'''
+    if ut_sqrt == 'svd':
+        pytest.skip("`svd_sqrt_diff` not implemented yet.")
+    
+    def ut_mean(x):
+        return ut.transform(lambda x: nlfunction(x, q), x, cov)[0]
+    
+    def ut_cov(x):
+        return ut.transform(lambda x: nlfunction(x, q), x, cov)[1]
+    
+    def f_diff(x, dx):
+        return np.einsum('i...k,k...j->i...j', dx, nlfunction.d_dx(x, q))
+    
+    num_mean_diff = utils.central_diff(ut_mean, x)
+    num_cov_diff = utils.central_diff(ut_cov, x)
+    
+    ut_mean(x)
+    in_mean_diff = np.identity(nx)
+    in_cov_diff = np.zeros((nx, nx, nx))
+    mean_diff, cov_diff = ut.transform_diff(f_diff, in_mean_diff, in_cov_diff)
+    
+    assert ArrayCmp(mean_diff, tol=1e-7) == num_mean_diff
+    assert ArrayCmp(cov_diff, tol=1e-5) == num_cov_diff
 
 
 class EulerDiscretizedAtmosphericReentry: