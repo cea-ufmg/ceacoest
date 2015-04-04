0eb2fd72feb8471ca37d3a16606f4a49e257a8e0
Dimas Abreu Dutra <dimasadutra@gmail.com>
2015-04-03 21:19:03 -0300

    sde: Added symbolic EulerDiscretization.

--- examples/duffing.py
+++ examples/duffing.py
@@ -3,16 +3,28 @@
 
 import numpy as np
 import sympy
-import sym2num.model
+import sym2num
 
 from qwfilter import sde
 
 
-class SymbolicDuffing(sym2num.model.SymbolicModel):
+class SymbolicDuffing(sde.EulerDiscretizedModel):
     '''Symbolic Duffing oscillator model.'''
+
+    var_names = ['t', 'x', 'y', 'q', 'c', 'td', 'k']
+    '''Name of model variables.'''
+    
+    function_names = ['f', 'g', 'h']
+    '''Name of the model functions.'''
     
     t = 't'
     '''Time variable.'''
+
+    k = 'k'
+    '''Discretized time index.'''
+    
+    td = 'Td'
+    '''Discretization period.'''
     
     x = ['x', 'v']
     '''State vector.'''
@@ -20,7 +32,7 @@ class SymbolicDuffing(sym2num.model.SymbolicModel):
     y = ['x_meas']
     '''Measurement vector.'''
     
-    q = ['alpha', 'beta', 'delta']
+    q = ['alpha', 'beta', 'delta', 'g1']
     '''Unknown parameter vector.'''
     
     c = ['gamma', 'omega']
@@ -28,13 +40,27 @@ class SymbolicDuffing(sym2num.model.SymbolicModel):
     
     def f(self, t, x, q, c):
         '''Drift function.'''
-        s = self.symbols(t, x, q, c)
-        f1 = s.xd
-        f2 = (-s.delta * s.xd - s.beta * s.x - s.alpha * s.x ** 3  +
+        s = self.symbols(t=t, x=x, q=q, c=c)
+        f1 = s.v
+        f2 = (-s.delta * s.v - s.beta * s.x - s.alpha * s.x ** 3  +
               s.gamma * sympy.cos(s.t * s.omega))
         return [f1, f2]
+
+    def g(self, t, x, q, c):
+        '''Diffusion matrix.'''
+        s = self.symbols(t=t, x=x, q=q, c=c)
+        return [[0], [s.g1]]
     
     def h(self, t, x, q, c):
         '''Measurement function.'''
         s = self.symbols(t, x, q, c)
         return [s.x]
+
+
+GeneratedDuffing = sym2num.class_obj(
+    SymbolicDuffing(), sym2num.ScipyPrinter(),
+    name='GeneratedDuffing', 
+    meta=sym2num.ParametrizedModel.meta
+)
+
+
