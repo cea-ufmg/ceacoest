5fcd06a7d293fa6d79aaf506bf0788e55ac140d5
Dimas Abreu Archanjo Dutra <dimasad@ufmg.br>
2018-08-13 13:45:33 -0300

    Developing sparse optimization problem modeling utilities.

--- ceacoest/optim.py
+++ ceacoest/optim.py
@@ -6,52 +6,64 @@ import collections
 import attrdict
 import numpy as np
 
-#### 
-#### Perhaps change the name of dvar to components and rethink terminology.
-#### The vector d has many X. Also intead of problem.decision it could be
-#### and the class Decision we could have something more akin to Spec, X-Spec.
-####
 
 class Problem:
     """Sparse optimization problem."""
     
     def __init__(self):
-        self.decision = collections.OrderedDict()
-        """Problem decision variable specifications."""
-
+        self.decision_components = collections.OrderedDict()
+        """Decision variable component specifications."""
+        
         self.nd = 0
-        """Total number of problem decision variables."""
+        """Total number of problem decision variable elements."""
     
     def register_decision_variable(self, name, shape):
-        size = np.prod(shape, dtype=int)
-        self.decision[name] = Decision(shape, self.nd)
-        self.nd += size
+        component = DecisionComponent(shape, self.nd)
+        self.decision_components[name] = component
+        self.nd += component.size
 
     def unpack_decision(self, dvec):
         """Unpack the vector of decision variables into its components."""
         dvec = np.asarray(dvec)
         assert d.shape == (self.nd,)
 
-        dvar = {}
-        for name, spec in self.decision.items():
-            dvar[name] = dvec[spec.slice].reshape(spec.shape)
-        return dvar
+        components = {}
+        for name, spec in self.decision_components.items():
+            components[name] = spec.extract_from(dvec)
+        return components
     
-    def pack_decision(self, dvar):
+    def pack_decision(self, **components):
         """Pack the decision variable components into the vector."""
-        dvec = np.empty(self.nd)
-        for name, spec in self.decision.items():
-            assert np.shape(dvar[name]) == spec.shape
-            dvec[spec.slice] = np.ravel(dvar[name])
+        dvec = np.zeros(self.nd)
+        for name, spec in self.decision_components.items():
+            spec.pack_into(dvec, components[name])
         return dvec
 
 
-class Decision:
+class DecisionComponent:
+    """Specificiation of a problem's decision variable component."""
+    
     def __init__(self, shape, offset):
         self.shape = shape
+        """The component's ndarray shape."""
+        
         self.offset = offset
-        self.size = size = np.prod(shape, dtype=int)
-        self.slice = slice(offset, offset + size)
+        """Offset into the decision vector."""
+        
+        self.size = np.prod(shape, dtype=int)
+        """Total number of elements."""
+        
+        self.slice = slice(offset, offset + self.size)
+        """This component's slice in the decision variables vector."""
+    
+    def unpack_from(self, dvec):
+        """Extract component from decicion variable vector."""
+        return np.asarray(dvec)[self.slice].reshape(self.shape)
+
+    def pack_into(self, dvec, value):
+        """Pack component into decicion variable vector."""
+        assert np.shape(value) == self.shape
+        dvec[self.slice] = np.ravel(value)
 
 
 Decision = collections.namedtuple('Decision', ['shape', 'offset', 'size'])
