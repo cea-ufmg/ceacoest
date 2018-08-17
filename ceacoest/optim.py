29bdfcfeadb917e25225f850d3370d3673753cf4
Dimas Abreu Archanjo Dutra <dimasad@ufmg.br>
2018-08-17 11:51:40 -0300

    Added constraint registration to `optim.Problem`.

--- ceacoest/optim.py
+++ ceacoest/optim.py
@@ -13,19 +13,29 @@ class Problem:
     def __init__(self):
         self.decision_components = collections.OrderedDict()
         """Decision variable component specifications."""
-        
-        self.nd = 0
-        """Total number of problem decision variable elements."""
-    
-    def register_decision_variable(self, name, shape, broadcast_dims=0):
-        component = DecisionComponent(shape, self.nd, broadcast_dims)
-        self.decision_components[name] = component
-        self.nd += component.size
 
+        self.ndec = 0
+        """Size of the decision vector."""
+
+        self.constraints = collections.OrderedDict()
+        """Problem constraints."""
+
+        self.ncons = 0
+        """Size of the constraint vector."""
+    
+    def register_decision(self, name, shape):
+        component = Component(shape, self.ndec)
+        self.decision_components[name] = component
+        self.ndec += component.size
+    
+    def register_derived(self, name, component):
+        """Register a variable derived from the decision variables."""
+        self.decision_components[name] = component
+    
     def unpack_decision(self, dvec):
         """Unpack the vector of decision variables into its components."""
         dvec = np.asarray(dvec)
-        assert d.shape == (self.nd,)
+        assert d.shape == (self.ndec,)
 
         components = {}
         for name, spec in self.decision_components.items():
@@ -34,61 +44,76 @@ class Problem:
     
     def pack_decision(self, **components):
         """Pack the decision variable components into the vector."""
-        dvec = np.zeros(self.nd)
-        for name, spec in self.decision_components.items():
-            spec.pack_into(dvec, components[name])
+        dvec = np.zeros(self.ndec)
+        for name, value in components:
+            self.decision_components[name].pack_into(dvec, value)
         return dvec
-
-    def register_constraint(self, name, shape, broadcast_dims=0):
-        pass
-
-
-class DecisionComponent:
-    """Specificiation of a problem's decision variable component."""
     
-    def __init__(self, shape, offset, broadcast_dims=0):
+    def register_constraint(self, name, shape, f, argument_names):
+        cons = Constraint(shape, self.ncons, f, argument_names)
+        self.constraints[name] = cons
+        self.ncons += cons.size
+        return cons
+    
+    def constraint(self, dvec):
+        dcomp = self.unpack_decision(dvec)
+        cvec = np.zeros(self.ncons)
+        for name, cons in self.constraints:
+            cons(dcomp, pack_into=cvec)
+        return cvec
+
+
+class Component:
+    """Specificiation of a problem's decision or constraint vector component."""
+    
+    def __init__(self, shape, offset=None, index=None):
         self.shape = shape
         """The component's ndarray shape."""
         
         self.offset = offset
-        """Offset into the decision vector."""
+        """Offset into the parent vector."""
         
         self.size = np.prod(shape, dtype=int)
         """Total number of elements."""
         
-        self.slice = slice(offset, offset + self.size)
-        """This component's slice in the decision variables vector."""
-
-        base_shape = shape[broadcast_dims:]
-        expansion_shape = shape[:broadcast_dims]
-        base_size = np.prod(base_shape, dtype=int)
-        expansion_size = np.prod(expansion_shape, dtype=int)
-        self.expansion_inds = (np.arange(expansion_size)[:, None]*base_size
-                               + offset)
-        """The offsets for index expansion."""
-
-        self.base_shape = base_shape
-        """Shape of the underlying variable, before broadcasting."""
+        if offset is not None and index is None:
+            index = slice(offset, offset + self.size)
+        self.index = index
+        """This component's index in the parent vector."""
     
-    def unpack_from(self, dvec):
-        """Extract component from decicion variable vector."""
-        return np.asarray(dvec)[self.slice].reshape(self.shape)
-
-    def pack_into(self, dvec, value):
-        """Pack component into decicion variable vector."""
-        if np.shape(value) != self.shape:
+    def unpack_from(self, vec):
+        """Extract component from parent vector."""
+        return np.asarray(vec)[self.index].reshape(self.shape)
+    
+    def pack_into(self, vec, value):
+        """Pack component into parent vector."""
+        value = np.asarray(value)
+        if value.shape != self.shape:
             try:
                 value = np.broadcast_to(value, self.shape)
             except ValueError:
-                value_shape = np.shape(value)
                 msg = "value with shape {} could not be broadcast to {}"
-                raise ValueError(msg.format(value_shape, self.shape))
-        dvec[self.slice] = np.ravel(value)
+                raise ValueError(msg.format(value.shape, self.shape))
+        vec[self.index].flat += value.flatten()
 
-    def expand_index(self, indices):
-        indices = np.asarray(indices, dtype=int)
-        return self.expansion_inds + indices
 
-    def expand_multi_index(self, multi_index):
-        indices = np.ravel_multi_index(multi_index, self.base_shape)
-        return self.expand_index(indices)
+class Constraint(Component):
+    def __init__(self, shape, offset, f, argument_names):
+        super.__init__(shape, offset)
+        
+        self.f = f
+        """Constraint function."""
+        
+        self.argument_names = argument_names
+        """Constraint function argument names."""
+        
+    def __call__(self, arg_dict, pack_into=None):
+        args = tuple(arg_dict[n] for n in self.argument_names)
+        ret = self.f(*args)
+        assert ret.shape == self.shape
+        
+        if pack_into is not None:
+            self.pack_into(pack_into, ret)
+        
+        return ret
+
