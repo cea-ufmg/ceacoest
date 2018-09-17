c67a2d403ef4b8495f94a55ad1fa5d45c303f9b7
Dimas Abreu Archanjo Dutra <dimasad@ufmg.br>
2018-09-17 12:35:42 -0300

    Creating baseclass for collocated model optimization.

--- ceacoest/oc.py
+++ ceacoest/oc.py
@@ -5,46 +5,23 @@ import itertools
 
 import numpy as np
 
-from . import optim, rk, utils
+from . import collocation, optim, utils
 
 
-class Problem(optim.Problem):
+class Problem(collocation.CollocatedProblem):
     """Optimal control problem with LGL direct collocation."""
     
     def __init__(self, model, t):
-        self.model = model
-        """Underlying cost, constraint and dynamical system model."""
-
-        self.collocation = col = rk.LGLCollocation(model.collocation_order)
-        """Collocation method."""
+        super().__init__(model, t)
+        npoints = self.npoints
+        npieces = self.npieces
         
-        self.piece_len = np.diff(t)
-        """Normalized length of each collocation piece."""
-
-        assert np.all(self.piece_len > 0)
-        self.npieces = npieces = len(self.piece_len)
-        """Number of collocation pieces."""
-        
-        self.tc = self.collocation.grid(t)
-        """Normalized collocation time grid."""
-
-        npoints = self.tc.size
-        self.npoints = npoints
-        """Total number of collocation points."""
-        
-        super().__init__()
-        x = self.register_decision('x', model.nx, npoints)
         u = self.register_decision('u', model.nu, npoints)
-        p = self.register_decision('p', model.np)
-        self.register_derived('xp', PieceRavelledVariable(self, 'x'))
-        self.register_derived('up', PieceRavelledVariable(self, 'u'))
+        self.register_derived('up', collocation.PieceRavelledVariable(self,'u'))
         self.register_derived('xe', XEVariable(self))
         
         self.register_constraint('g', model.g, ('x','u','p'), model.ng, npoints)
         self.register_constraint('h', model.h, ('xe', 'p'), model.nh)
-        self.register_constraint(
-            'e', model.e, ('xp','up','p', 'piece_len'), model.ne, npieces
-        )
         self._register_model_constraint_derivatives('g', ('x', 'u', 'p'))
         self._register_model_constraint_derivatives('h', ('xe', 'p'))
         self._register_model_constraint_derivatives('e', ('xp', 'up', 'p'))
@@ -55,98 +32,10 @@ class Problem(optim.Problem):
         )
         self._register_model_merit_derivatives('M', ('xe', 'p'))
         self._register_model_merit_derivatives('IL', ('xp', 'up', 'p'))
-    
-    def _register_model_merit_gradient(self, merit_name, wrt_name):
-        grad = getattr(self.model, f'd{merit_name}_d{wrt_name}')
-        self.register_merit_gradient(merit_name, wrt_name, grad)
-
-    def _register_model_merit_hessian(self, merit_name, wrt_names):
-        hess_name = utils.double_deriv_name(merit_name, wrt_names)
-        val = getattr(self.model, f'{hess_name}_val')
-        ind = getattr(self.model, f'{hess_name}_ind')
-        self.register_merit_hessian(merit_name, wrt_names, val, ind)
-
-    def _register_model_merit_derivatives(self, merit_name, wrt_names):
-        for wrt_name in wrt_names:
-            self._register_model_merit_gradient(merit_name, wrt_name)
-        for comb in itertools.combinations_with_replacement(wrt_names, 2):
-            self._register_model_merit_hessian(merit_name, comb)
-    
-    def _register_model_constraint_jacobian(self, constraint_name, wrt_name):
-        val = getattr(self.model, f'd{constraint_name}_d{wrt_name}_val')
-        ind = getattr(self.model, f'd{constraint_name}_d{wrt_name}_ind')
-        self.register_constraint_jacobian(constraint_name, wrt_name, val, ind)
-
-    def _register_model_constraint_hessian(self, constraint_name, wrt_names):
-        hess_name = utils.double_deriv_name(constraint_name, wrt_names)
-        val = getattr(self.model, f'{hess_name}_val')
-        ind = getattr(self.model, f'{hess_name}_ind')
-        self.register_constraint_hessian(constraint_name, wrt_names, val, ind)
-    
-    def _register_model_constraint_derivatives(self, cons_name, wrt_names):
-        for wrt_name in wrt_names:
-            self._register_model_constraint_jacobian(cons_name, wrt_name)
-        for comb in itertools.combinations_with_replacement(wrt_names, 2):
-            self._register_model_constraint_hessian(cons_name, comb)
-        
+            
     def variables(self, dvec):
         """Get all variables needed to evaluate problem functions."""
         return {'piece_len': self.piece_len, **super().variables(dvec)}
-    
-    def set_decision_item(self, name, value, dvec):
-        self._set_decision_item(name, value, self.model.symbol_index_map, dvec)
-
-    def set_defect_scale(self, name, value, cvec):
-        component_name, index = self.model.symbol_index_map[name]
-        if component_name != 'x':
-            raise ValueError(f"'{name}' is not a component of the state vector")
-        e = self.constraints['e'].unpack_from(cvec)
-        e = e.reshape((self.npieces, self.collocation.ninterv, self.model.nx))
-        e[(..., *index)] = value
-
-
-class PieceRavelledVariable:
-    def __init__(self, problem, var_name):
-        self.p = problem
-        self.var_name = var_name
-    
-    @property
-    def var(self):
-        return self.p.decision[self.var_name]
-    
-    @property
-    def nvar(self):
-        return self.var.shape[1]
-    
-    @property
-    def shape(self):
-        return (self.p.npieces, self.p.collocation.n, self.nvar)
-
-    @property
-    def tiling(self):
-        return self.p.npieces
-    
-    def build(self, variables):
-        v = variables[self.var_name]
-        assert v.shape == self.var.shape
-        vp = np.zeros(self.shape)
-        vp[:, :-1].flat = v[:-1, :].flat
-        vp[:-1, -1] = vp[1:, 0]
-        vp[-1, -1] = v[-1]
-        return vp
-    
-    def add_to(self, destination, value):
-        vp = np.asarray(value)
-        assert vp.shape == self.shape
-        v = np.zeros(self.var.shape)
-        v[:-1].flat = vp[:, :-1].flatten()
-        v[self.p.collocation.n-1::self.p.collocation.n-1] += vp[:, -1]
-        self.var.add_to(destination, v)
-    
-    def expand_indices(self, ind):
-        npieces = self.p.npieces
-        increments = self.p.collocation.ninterv * self.nvar
-        return ind + np.arange(npieces)[:, None] * increments + self.var.offset
 
 
 class XEVariable:
