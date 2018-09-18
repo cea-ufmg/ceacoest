76bffddd2d05050e352612577512dc522e33f165
Dimas Abreu Archanjo Dutra <dimasad@ufmg.br>
2018-09-18 16:15:51 -0300

    HFB320 example now fully functional.

--- examples/hfb320_long_oem.py
+++ examples/hfb320_long_oem.py
@@ -16,6 +16,7 @@ import numpy as np
 import sympy
 import sym2num.model
 import sym2num.utils
+from scipy import integrate, interpolate
 from sym2num import var
 from sympy import cos, sin
 
@@ -26,7 +27,7 @@ from ceacoest.modelling import symcol, symoem, symstats
 import imp; [imp.reload(m) for m in [symcol, symoem, oem]]
 
 
-@symoem.collocate(order=2)
+@symoem.collocate(order=3)
 class HFB320Long:
     """Symbolic HFB-320 aircraft nonlinear longitudinal model."""
     
@@ -101,6 +102,20 @@ class HFB320Long:
         )
 
 
+def set_xguess(problem, u, dec0):
+    p0 = problem.variables(dec0)['p']
+    tspan = problem.tc[[0, -1]]
+    x0 = problem.y[0, :4]
+    
+    def xdot(t, x):
+        return problem.model.f(x, u(t), p0)
+    
+    sol = integrate.solve_ivp(xdot, tspan, x0, max_step=0.05)
+    
+    x = interpolate.interp1d(sol.t, sol.y)(problem.tc).T
+    problem.set_decision('x', x, dec0)
+
+
 if __name__ == '__main__':
     given = {'g0': 9.80665, 'Sbym': 4.0280e-3, 'ScbyIy': 8.0027e-4, 
              'FEIYLT': -7.0153e-6, 'V0': 104.67, 'mass':7472, 'sigmaT':0.0524,
@@ -115,7 +130,7 @@ if __name__ == '__main__':
     Ts = 0.1
     t = np.arange(len(data)) * Ts
     y = data[:, 4:11]
-    u = data[:, [1,3]]
+    u = interpolate.interp1d(t, data[:, [1,3]], axis=0)
     problem = oem.Problem(model, t, y, u)
     
     dec_L, dec_U = np.repeat([[-np.inf], [np.inf]], problem.ndec, axis=-1)
@@ -127,18 +142,42 @@ if __name__ == '__main__':
     problem.set_decision_item('qdot_meas_std', 1e-4, dec_L)
     problem.set_decision_item('ax_meas_std', 1e-4, dec_L)
     problem.set_decision_item('az_meas_std', 1e-4, dec_L)
-
-    dec0 = np.zeros(problem.ndec)
-    problem.set_decision('x', y[:, :4], dec0)
-    problem.set_decision('p', 1, dec0)
     
     constr_bounds = np.zeros((2, problem.ncons))
+    
+    guess = dict(CD0=5.67516e-03, CDV=7.72612e-03, CDa=6.74256e-01,
+                 CL0=-3.18267e-01, CLV=2.60317e-01, CLa=5.37576, 
+                 Cm0=4.98218e-02, CmV=1.89182e-02, Cma=-4.98586e-1, 
+                 Cmq=-2.58439e1, Cmde=-9.90687e-01)
+    dec0 = np.zeros(problem.ndec)
+    problem.set_decision_item('V_meas_std', 0.2, dec0)
+    problem.set_decision_item('alpha_meas_std', 0.03, dec0)
+    problem.set_decision_item('theta_meas_std', 0.002, dec0)
+    problem.set_decision_item('q_meas_std', 0.001, dec0)
+    problem.set_decision_item('qdot_meas_std', 0.025, dec0)
+    problem.set_decision_item('ax_meas_std', 0.03, dec0)
+    problem.set_decision_item('az_meas_std', 0.03, dec0)
+    for k,v in guess.items():
+        problem.set_decision_item(k, v, dec0)    
+    set_xguess(problem, u, dec0)
+    
+    dec_scale = np.ones(problem.ndec)
+    problem.set_decision_item('V', 1e-2, dec_scale)
+    problem.set_decision_item('alpha', 20, dec_scale)
+    problem.set_decision_item('q', 30, dec_scale)
+    problem.set_decision_item('theta', 20, dec_scale)
+    
+    constr_scale = np.ones(problem.ncons)
+    problem.set_defect_scale('V', 1e-2, constr_scale)
+    problem.set_defect_scale('alpha', 20, constr_scale)
+    problem.set_defect_scale('q', 30, constr_scale)
+    problem.set_defect_scale('theta', 20, constr_scale)
     
     with problem.ipopt((dec_L, dec_U), constr_bounds) as nlp:
         nlp.add_str_option('linear_solver', 'ma57')
         nlp.add_num_option('tol', 1e-6)
         nlp.add_int_option('max_iter', 1000)
-        #nlp.set_scaling(1e-2, dec_scale, constr_scale)
+        nlp.set_scaling(-1, dec_scale, constr_scale)
         decopt, info = nlp.solve(dec0)
     
     opt = problem.variables(decopt)
